8e740ed9e89bebe50d68d7c2054463d65a8b4c02
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-05 23:03:52 +0100

    Added common target for common code

---
+++ src/common/factory_functions.hpp
@@ -0,0 +1,15 @@
+#ifndef FACTORY_FUNCTIONS_HPP
+#define FACTORY_FUNCTIONS_HPP
+
+#include <vector>
+
+namespace factory_funcs
+{
+
+template <typename T> int vectorsizeof(const std::vector<T> &vec)
+{
+    return sizeof(T) * vec.size();
+}
+} // namespace factory_funcs
+
+#endif // FACTORY_FUNCTIONS_HPP
