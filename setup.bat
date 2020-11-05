8a5e2d8722c85cef84b926e23e3274cac3b45a62
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-05 22:52:22 +0100

    Added building Release and Debug at setup

--- setup.bat
+++ setup.bat
@@ -2,6 +2,7 @@ rmdir /q/s build
 mkdir build
 cd build
 cmake ..
-start cmake --build . -j
+start cmake --build . -j --config Release
+start cmake --build . -j --config Debug
 start polyphase_sort.sln
 cd ..
