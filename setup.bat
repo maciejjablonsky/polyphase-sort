a307d996944d38fe15a4251918a292296bbf9a85
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-06 09:32:47 +0100

    Testing set by default in setup script

--- setup.bat
+++ setup.bat
@@ -1,8 +1,9 @@
 rmdir /q/s build
 mkdir build
 cd build
-cmake ..
+cmake .. -DTESTING=1
 start cmake --build . -j --config Release
 start cmake --build . -j --config Debug
 start polyphase_sort.sln
+start code ..
 cd ..
