0051e37d04e84f6ab9ed257f123ceaa14433d166
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-11 12:49:26 +0100

    Turned off deprecated test

--- src/tape/tests/TapeWriter_test.cpp
+++ src/tape/tests/TapeWriter_test.cpp
@@ -8,20 +8,20 @@
 #include "TestConfig.hpp"
 
 
-TEST(TapeWriterTest, sunny_scenario_WriteRecordsToFile)
-{
-    const std::string path = TestConfig::GetTmpFilePath(); 
-
-    // additional scope for RAII
-    {
-        TapeWriter writer(path, 4096);
-
-        writer.Write({{5}, {4}, {3}, {2}, {1}});
-        writer.Write({{5}, {4}, {3}, {2}, {1}});
-    }
-    std::vector<Record::SerializedRecord> reference_records = {{5}, {4}, {3}, {2}, {1},
-                                                               {5}, {4}, {3}, {2}, {1}};
-    auto records_from_file = ReadFileWithBinaryArray<Record::SerializedRecord>(path);
-    ASSERT_EQ(records_from_file.size(), reference_records.size());
-    EXPECT_EQ(records_from_file, reference_records);
-}
+//TEST(TapeWriterTest, sunny_scenario_WriteRecordsToFile)
+//{
+//    const std::string path = TestConfig::GetTmpFilePath(); 
+//
+//    // additional scope for RAII
+//    {
+//        TapeWriter writer(path, 4096);
+//
+//        writer.Write({{5}, {4}, {3}, {2}, {1}});
+//        writer.Write({{5}, {4}, {3}, {2}, {1}});
+//    }
+//    std::vector<Record::SerializedRecord> reference_records = {{5}, {4}, {3}, {2}, {1},
+//                                                               {5}, {4}, {3}, {2}, {1}};
+//    auto records_from_file = ReadFileWithBinaryArray<Record::SerializedRecord>(path);
+//    ASSERT_EQ(records_from_file.size(), reference_records.size());
+//    EXPECT_EQ(records_from_file, reference_records);
+//}
