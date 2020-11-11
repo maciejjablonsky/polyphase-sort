9826cf92ba923ed415fedecc4e333dd54368f241
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-11 12:05:14 +0100

    Fixed PageWriter to accept pages, buffer and write them on flush

--- src/tape/tests/PageWriter_test.cpp
+++ src/tape/tests/PageWriter_test.cpp
@@ -7,35 +7,39 @@
 
 TEST(PageWriterTest, sunny_scenario_BytesLengthNotExceeding)
 {
-    const std::string tape_path = TestConfig::GetTmpFilePath(); 
-    Page page{0x4_byte, 0_byte, 0_byte, 0_byte,0_byte, 0_byte, 0_byte,0_byte, 0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte};
+    const std::string tape_path = TestConfig::GetTmpFilePath();
+    Page page{0x4_byte, 0_byte, 0_byte,   0_byte,   0_byte,   0_byte,
+              0_byte,   0_byte, 0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte};
     const int page_size = 12;
     PageWriter writer(tape_path, page_size);
     ASSERT_EQ(page.size(), page_size);
     EXPECT_EQ(page.records_number(), 4);
-    EXPECT_NO_THROW(writer.WritePage(page));
+    EXPECT_NO_THROW(writer.WritePage(std::move(page)));
 }
 
 TEST(PageWriterTest, BytesLengthExceedPageSize)
 {
     const std::string tape_path = TestConfig::GetTmpFilePath();
-    Page page{0x5_byte, 0_byte, 0_byte, 0_byte, 0_byte, 0_byte, 0_byte, 0_byte, 0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte, 0x5_byte};
+    Page page{0x5_byte, 0_byte,   0_byte,   0_byte,   0_byte,   0_byte,  0_byte,
+              0_byte,   0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte, 0x5_byte};
     const auto page_size = 12;
     PageWriter writer(tape_path, page_size);
     ASSERT_EQ(page.size(), page_size + 1);
     EXPECT_EQ(page.records_number(), 5);
-    EXPECT_ANY_THROW(writer.WritePage(page));
+    EXPECT_ANY_THROW(writer.WritePage(std::move(page)));
 }
 
 TEST(PageWriterTest, DriveAccessCounterWorks)
 {
     const std::string tape_path = TestConfig::GetTmpFilePath();
-    Page page{0x4_byte, 0_byte, 0_byte, 0_byte,0_byte, 0_byte, 0_byte,0_byte, 0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte};
+    Page page{0x4_byte, 0_byte, 0_byte,   0_byte,   0_byte,   0_byte,
+              0_byte,   0_byte, 0x1_byte, 0x2_byte, 0x3_byte, 0x4_byte};
     const auto page_size = 12;
     PageWriter writer(tape_path, page_size);
     ASSERT_EQ(page.size(), page_size);
-    
+
     EXPECT_EQ(writer.GetHardDriveAccessesNumber(), 0);
-    writer.WritePage(page);
+    writer.WritePage(std::move(page));
+    writer.Flush();
     EXPECT_EQ(writer.GetHardDriveAccessesNumber(), 1);
 }
