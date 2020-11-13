8593d77749260d8492f23db1c48b59e01c014cc9
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-13 22:31:51 +0100

    Added RecordsPage

---
+++ src/tape/src/RecordsPage.cpp
@@ -0,0 +1,65 @@
+#include "RecordsPage.hpp"
+#include <stdexcept>
+
+RecordsPage::const_iterator::const_iterator(const Record::SerializedRecord* ptr, const RecordsPage& parent)
+    : parent_(&parent), ptr_(ptr)
+{
+}
+
+RecordsPage::const_iterator& RecordsPage::const_iterator::operator++()
+{
+    if (records_idx_ >= parent_->page_header_->inserted_records_number)
+    {
+        throw std::out_of_range("No more records on the page.");
+    }
+    ++records_idx_;
+    ++ptr_;
+}
+
+const RecordsPage::stored_item& RecordsPage::const_iterator::operator*()
+{
+    return *ptr_;
+}
+
+const RecordsPage::stored_item& RecordsPage::const_iterator::operator->()
+{
+    return *ptr_;
+}
+
+bool operator==(const RecordsPage::const_iterator& lhs, const RecordsPage::const_iterator& rhs)
+{
+    return lhs.ptr_ == rhs.ptr_;
+}
+
+bool operator!=(const RecordsPage::const_iterator& lhs, const RecordsPage::const_iterator& rhs)
+{
+    return !(lhs == rhs);
+}
+
+RecordsPage::RecordsPage(Page&& page)
+    : page_(std::move(page)), page_header_(reinterpret_cast<PageHeader*>(page_.data()))
+{
+}
+
+RecordsPage::const_iterator RecordsPage::cbegin()
+{
+    return {reinterpret_cast<const Record::SerializedRecord*>(page_.data() + sizeof(PageHeader)), *this};
+}
+
+RecordsPage::const_iterator RecordsPage::begin() const
+{
+    return const_cast<RecordsPage&>(*this).cbegin();
+}
+
+RecordsPage::const_iterator RecordsPage::cend()
+{
+    return {reinterpret_cast<const Record::SerializedRecord*>(page_.data() + sizeof(PageHeader) +
+                                                              page_header_->inserted_records_number *
+                                                                  sizeof(Record::SerializedRecord)),
+            *this};
+}
+
+RecordsPage::const_iterator RecordsPage::end() const
+{
+    return const_cast<RecordsPage&>(*this).cend();
+}
---
+++ src/tape/src/RecordsPage.hpp
@@ -0,0 +1,40 @@
+#ifndef RECORDS_PAGE_HPP
+#define RECORDS_PAGE_HPP
+
+#include "Page.hpp"
+#include "Record.hpp"
+
+class RecordsPage
+{
+  public:
+    using stored_item = Record::SerializedRecord;
+
+    class const_iterator
+    {
+      public:
+        const_iterator(const Record::SerializedRecord * ptr, const RecordsPage& parent);
+        const_iterator& operator++();
+        const RecordsPage::stored_item& operator*();
+        const RecordsPage::stored_item& operator->();
+
+        friend bool operator==(const const_iterator& lhs, const const_iterator& rhs);
+        friend bool operator!=(const const_iterator& lhs, const const_iterator& rhs);
+
+      private:
+        const RecordsPage* parent_ = nullptr;
+        const Record::SerializedRecord* ptr_ = nullptr;
+        uint64_t records_idx_ = 0;
+    };
+    
+    RecordsPage(Page&& page);
+    const_iterator cbegin();
+    const_iterator begin() const;
+    const_iterator cend();
+    const_iterator end() const;
+
+  private:
+    PageHeader* const page_header_;
+    Page page_;
+};
+
+#endif // RECORDS_PAGE_HPP
