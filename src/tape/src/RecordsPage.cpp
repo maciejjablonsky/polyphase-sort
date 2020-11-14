4ce76a94176f1b0462abd6225e7825fe38a6331a
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-14 09:39:32 +0100

    Added RecordsPage write iterator

--- src/tape/src/RecordsPage.cpp
+++ src/tape/src/RecordsPage.cpp
@@ -64,3 +64,65 @@ RecordsPage::const_iterator RecordsPage::end() const
 {
     return const_cast<RecordsPage&>(*this).cend();
 }
+
+RecordsPage::iterator RecordsPage::begin()
+{
+    return {reinterpret_cast<Record::SerializedRecord*>(page_.data() + sizeof(PageHeader)), *this};
+}
+
+RecordsPage::iterator RecordsPage::end()
+{
+    return {reinterpret_cast<Record::SerializedRecord*>(
+                page_.data() + sizeof(PageHeader) +
+                ((page_.size() - sizeof(PageHeader)) / sizeof(Record::SerializedRecord)) *
+                    sizeof(Record::SerializedRecord)),
+            *this};
+}
+
+RecordsPage::operator Page()
+{
+    return page_;
+}
+
+RecordsPage& RecordsPage::operator=(Page&& page)
+{
+    page_ = std::move(page);
+    page_header_ = reinterpret_cast<PageHeader*>(page_.data());
+    return *this;
+}
+
+void RecordsPage::WriteStoredRecordsNumber(uint64_t records)
+{
+    page_header_->inserted_records_number = records;
+}
+
+RecordsPage::iterator::iterator(Record::SerializedRecord* ptr, RecordsPage& parent)
+    : parent_(&parent), ptr_(ptr)
+{
+}
+
+RecordsPage::iterator& RecordsPage::iterator::operator++()
+{
+    ++ptr_;
+    return* this;
+}
+
+RecordsPage::stored_item& RecordsPage::iterator::operator*()
+{
+    return *ptr_;
+}
+
+RecordsPage::stored_item& RecordsPage::iterator::operator->()
+{
+    return **this;
+}
+
+bool operator==(const RecordsPage::iterator & lhs, const RecordsPage::iterator & rhs)
+{
+    return lhs.ptr_ == rhs.ptr_;
+}
+
+bool operator!=(const RecordsPage::iterator& lhs, const RecordsPage::iterator& rhs)
+{
+    return !(lhs == rhs);
+}
--- src/tape/src/RecordsPage.hpp
+++ src/tape/src/RecordsPage.hpp
@@ -3,16 +3,34 @@
 
 #include "Page.hpp"
 #include "Record.hpp"
+#include <cstdint>
 
 class RecordsPage
 {
   public:
     using stored_item = Record::SerializedRecord;
 
+    class iterator
+    {
+      public:
+        iterator(Record::SerializedRecord* ptr, RecordsPage& parent);
+        iterator& operator++();
+        RecordsPage::stored_item& operator*();
+        RecordsPage::stored_item& operator->();
+
+        friend bool operator==(const iterator& lhs, const iterator& rhs);
+        friend bool operator!=(const iterator& lhs, const iterator& rhs);
+
+      private:
+        RecordsPage* parent_ = nullptr;
+        Record::SerializedRecord* ptr_ = nullptr;
+        uint64_t records_idx_ = 0;
+    };
+
     class const_iterator
     {
       public:
-        const_iterator(const Record::SerializedRecord * ptr, const RecordsPage& parent);
+        const_iterator(const Record::SerializedRecord* ptr, const RecordsPage& parent);
         const_iterator& operator++();
         const RecordsPage::stored_item& operator*();
         const RecordsPage::stored_item& operator->();
@@ -25,12 +43,25 @@ class RecordsPage
         const Record::SerializedRecord* ptr_ = nullptr;
         uint64_t records_idx_ = 0;
     };
-    
+
+    static_assert(std::is_copy_constructible<const_iterator>());
+    static_assert(std::is_copy_assignable<const_iterator>());
+    static_assert(std::is_destructible<const_iterator>());
+    static_assert(std::is_swappable<const_iterator>());
+    static_assert(std::is_copy_constructible<iterator>());
+    static_assert(std::is_copy_assignable<iterator>());
+    static_assert(std::is_destructible<iterator>());
+    static_assert(std::is_swappable<iterator>());
+
     RecordsPage(Page&& page);
     const_iterator cbegin();
     const_iterator begin() const;
     const_iterator cend();
     const_iterator end() const;
+    iterator begin();
+    iterator end();
+
+    void WriteStoredRecordsNumber(uint64_t records);
 
   private:
     Page page_;
