cfe17ef125f1b81549dc65c78014093c49b733c1
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-14 10:01:58 +0100

    Replaced raw Page with RecordsPage and removed buffering from TapeWriter

--- src/tape/src/TapeWriter.cpp
+++ src/tape/src/TapeWriter.cpp
@@ -3,27 +3,28 @@
 #include <fmt/format.h>
 
 TapeWriter::TapeWriter(const std::string_view tape_path, int disk_page_size)
-    : writer_(tape_path, disk_page_size), page_size_(disk_page_size), records_count_(0), preparing_page_(disk_page_size)
+    : writer_(tape_path, disk_page_size), page_size_(disk_page_size), preparing_page_(disk_page_size),
+      preparing_page_iterator_(preparing_page_.begin())
 {
 }
 
-void TapeWriter::Write(const Record::SerializedRecord &record)
+void TapeWriter::Write(const Record::SerializedRecord& record)
 {
 
-    preparing_page_.Append(reinterpret_cast<const std::byte *>(&record), sizeof(record));
-    ++records_count_;
-
-    if (preparing_page_.IsFull())
+    if (preparing_page_iterator_ == preparing_page_.end())
     {
-        preparing_page_.SetRecordsNumber(records_count_);
-        writer_.WritePage(std::move(preparing_page_));
-        preparing_page_ = Page(page_size_);
-        records_count_ = 0;
+        Flush();
+        PrepareNewPage();
     }
+
+    *preparing_page_iterator_++ = record;
+    ++records_count_;
 }
 
 void TapeWriter::Flush()
 {
+    preparing_page_.WriteStoredRecordsNumber(records_count_);
+    writer_.WritePage(std::move(preparing_page_));
     writer_.Flush();
 }
 
@@ -31,11 +32,18 @@ TapeWriter::~TapeWriter()
 {
     try
     {
-        Flush(); 
+        Flush();
     }
     catch (const std::exception& e)
     {
         fmt::print("Failed to print records to tape. Error message: {}", e.what());
         std::exit(-1);
     }
+}
+
+void TapeWriter::PrepareNewPage()
+{
+    preparing_page_ = Page(page_size_);
+    preparing_page_iterator_ = preparing_page_.begin();
+    records_count_ = 0;
 }
