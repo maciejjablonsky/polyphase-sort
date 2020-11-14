7e46f911321c473db0c8b389e7671093c7920423
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-14 09:59:09 +0100

    Enabled TapeWriter tests

--- src/tape/tests/TapeWriter_test.cpp
+++ src/tape/tests/TapeWriter_test.cpp
@@ -14,20 +14,23 @@ TEST(TapeWriterTest, sunny_scenario_WriteRecordsToFile)
         {0x0111111111111111}, {0x0222222222222222}, {0x0333333333333333}, {0x0444444444444444},
         {0x0555555555555555}, {0x0666666666666666}, {0x0777777777777777}, {0x0888888888888888},
         {0x0999999999999999}, {0x0AAAAAAAAAAAAAAA}};
+    uint64_t page_size = (reference_records.size() * sizeof(Record::SerializedRecord) / 2) + sizeof(PageHeader);
+
     // additional scope for RAII
     {
-        TapeWriter writer(path, 5 * sizeof(Record::SerializedRecord) + sizeof(PageHeader));
+        TapeWriter writer(path, page_size);
 
-        for (const auto &record : reference_records)
+        for (const auto& record : reference_records)
         {
             writer.Write(record);
         }
     }
     auto file_content = ReadBinaryFile(path);
-    ASSERT_EQ(file_content.size(), reference_records.size() * sizeof(Record::SerializedRecord) + 2*sizeof(PageHeader));
+    ASSERT_EQ(file_content.size(),
+              reference_records.size() * sizeof(Record::SerializedRecord) + 2 * sizeof(PageHeader));
 
-    TapeReader tape_reader(path, 5 * sizeof(Record::SerializedRecord) + sizeof(PageHeader));
+    TapeReader tape_reader(path, page_size);
     std::vector<Record::SerializedRecord> records_from_file(reference_records.size());
-    std::copy_n(tape_reader.begin(), reference_records.size(), records_from_file.begin());
+    std::copy_n(tape_reader.cbegin(), reference_records.size(), records_from_file.begin());
     EXPECT_EQ(reference_records, records_from_file);
 }
