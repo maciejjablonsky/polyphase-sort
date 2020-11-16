e97ad048767afe6b1d385eaf99dfc071297e803c
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-17 00:45:02 +0100

    Added generating tapes with n series inside

--- src/tape_generator/TapeGenerator.cpp
+++ src/tape_generator/TapeGenerator.cpp
@@ -4,9 +4,12 @@
 #include <iostream>
 #include "Record.hpp"
 #include "TapeWriter.hpp"
+#include <limits>
+#include <ctime>
+#include <cstdlib>
 
 template <typename T>
-std::optional<T> get_optional_argument_value(const std::vector<std::string_view> &argv,
+std::optional<T> get_optional_argument_value(const std::vector<std::string_view>& argv,
                                              std::string_view command)
 {
     auto command_iter = std::find(argv.begin(), argv.end(), command);
@@ -23,28 +26,37 @@ std::optional<T> get_optional_argument_value(const std::vector<std::string_view>
     }
     else
     {
-        value = command_value; 
+        value = command_value;
     }
     return std::make_optional(value);
 }
 
-TapeGenerator::TapeGenerator(const int argc, const char *argv[]) : argv_{argv + 1, argv + argc}
+TapeGenerator::TapeGenerator(const int argc, const char* argv[]) : argv_{argv + 1, argv + argc}
 {
-    auto records_number = GetNumberOfRecordsToGenerate();
-    auto out_file_path = GetOuputFilePath();
-    if (!records_number || !out_file_path)
+    auto is_records_number = GetNumberOfRecordsToGenerate();
+    auto is_out_file_path = GetOuputFilePath();
+    auto is_page_size = GetPageSize();
+    auto is_series_num = GetSeriesNum();
+    try
     {
-        fmt::print("Usage: tape_generator {} <number> [{} <file path>] [{} <page size>]\n",
-                   RECORD_NUM_COMMAND, OUTPUT_FILE_COMMAND, PAGE_SIZE_COMMAND);
-        throw std::runtime_error("Invalid arguments.");
+        if ((is_records_number == is_series_num))
+        {
+            throw std::runtime_error("You can use only records_number or series_number");
+        }
+        if (!is_out_file_path)
+        {
+            throw std::runtime_error("You need to provide output file path");
+        }
     }
-    number_of_records_ = *records_number;
-    out_file_path_ = *out_file_path;
-
-    auto page_size = GetPageSize();
-    if (page_size)
+    catch (const std::exception& e)
     {
-        page_size_ = *page_size;
+        PrintUsage();
+        fmt::print("Error: {}\n", e.what());
+        std::exit(-1);
+    }
+    if (is_page_size)
+    {
+        page_size_ = *is_page_size;
     }
 }
 
@@ -63,18 +75,60 @@ std::optional<int> TapeGenerator::GetPageSize() const
     return get_optional_argument_value<int>(argv_, TapeGenerator::PAGE_SIZE_COMMAND);
 }
 
+std::optional<int> TapeGenerator::GetSeriesNum() const
+{
+    return get_optional_argument_value<int>(argv_, TapeGenerator::SERIES_NUM_COMMAND);
+}
+
 void TapeGenerator::Generate() const
 {
-    TapeWriter writer(out_file_path_, page_size_);
-    for (int i = 0; i < number_of_records_; ++i)
+
+    if (auto opt_records = GetNumberOfRecordsToGenerate(); opt_records)
+    {
+        GenerateRecords(*opt_records);
+    }
+    else if (auto opt_series = GetSeriesNum(); opt_series)
+    {
+        GenerateSeries(*opt_series);
+    }
+}
+
+void TapeGenerator::PrintUsage() const
+{
+    fmt::print("Usage: tape_generator [{} <number> | {} <number>] [{} <file path>] [{} <page size>]\n",
+               RECORD_NUM_COMMAND, SERIES_NUM_COMMAND, OUTPUT_FILE_COMMAND, PAGE_SIZE_COMMAND);
+}
+
+void TapeGenerator::GenerateRecords(int records_num) const
+{
+    auto out_file_path = *GetOuputFilePath();
+    TapeWriter writer(out_file_path, page_size_);
+    for (int i = 0; i < records_num; ++i)
     {
         auto record = Record::GetRandom();
         writer.Write(Record::Serialize(record));
-        if (i % 500000 == 0)
-        {
-            writer.Flush(); 
-        }
     }
-    fmt::print("Written {} records to file {}. Page size is {}.\n", number_of_records_, out_file_path_,
-               page_size_);
+    fmt::print("Written {} records to file {}. Page size is {}.\n", records_num, out_file_path, page_size_);
+}
+
+void TapeGenerator::GenerateSeries(int series_num) const
+{
+    std::srand(time(nullptr));
+    auto out_file_path = *GetOuputFilePath();
+    TapeWriter writer(out_file_path, page_size_);
+    Record::RuntimeRecord last_series_start = {0xFF};
+    for (int s = 0; s < series_num; ++s)
+    {
+        int records_in_series = (std::rand() % (TapeGenerator::max_records_in_series_ -
+                                                TapeGenerator::min_records_in_series_ + 1)) +
+                                TapeGenerator::min_records_in_series_;
+        auto record = last_series_start + 0x8;
+        for (int r = 0; r < records_in_series; ++r)
+        {
+            writer.Write(Record::Serialize(record));
+            record += 0xF;
+        }
+        last_series_start += 0x2;
+    }
+    fmt::print("Written {} series to file {}. Page size is {}.\n", series_num, out_file_path, page_size_);
 }
