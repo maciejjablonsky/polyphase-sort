eac279ad6e61f8903c57cb62e57db00e662392a1
Maciej Jablonski <maciej.jablonski@intel.com>
2020-11-17 00:49:41 +0100

    Added test for Page moving

--- src/tape/tests/Page_test.cpp
+++ src/tape/tests/Page_test.cpp
@@ -27,3 +27,24 @@ TEST(PageWritingTest, sunny_scenario_ProperWrite)
         ASSERT_EQ(byte, vec[i++]);
     }
 }
+
+TEST(PageTest, sunny_scenario_MoveWorks)
+{
+    byte_vector vec = {0x1_b, 0x2_b, 0x3_b, 0x4_b, 0x5_b};
+    Page page(vec.size());
+    int i = 0;
+    for (auto it = page.begin(); i < 2; ++i, ++it)
+    {
+        *it = vec[i];
+    }
+    Page new_page = std::move(page);
+    for (auto it = new_page.begin() + i; i < vec.size(); ++i, ++it)
+    {
+        *it = vec[i];
+    }
+
+    for (int i = 0; const auto& byte : new_page)
+    {
+        ASSERT_EQ(byte, vec[i++]);
+    }
+}
